4b9cac834e5ba189c2d6afb29448b372e06c8916
Yuhei Kudo <yuhei-kudo@mbp2023.local>
2025-09-27 23:49:20 +0900

    practice 1-2

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -15,8 +16,30 @@ func main() {
 		io.WriteString(w, "Posting Article...\n")
 	}
 
+	listArticlesHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Article List\n")
+	}
+
+	getArticleHandler := func(w http.ResponseWriter, req *http.Request) {
+		articleID := 1
+		resString := fmt.Sprintf("Article No.%d\n", articleID)
+		io.WriteString(w, resString)
+	}
+
+	niceArticleHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Nice...\n")
+	}
+
+	commentArticleHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Comment...\n")
+	}
+
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/article", postArticleHandler)
+	http.HandleFunc("/article/list", listArticlesHandler)
+	http.HandleFunc("/article/1", getArticleHandler)
+	http.HandleFunc("/article/nice", niceArticleHandler)
+	http.HandleFunc("/comment", commentArticleHandler)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
