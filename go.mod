43531c8a4585ed2620f5259d531f9473dd45fa8e
Yuhei Kudo <yuhei-kudo@mbp2023.local>
2025-09-28 16:40:07 +0900

    lesson 1-4

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kdyhyhy2343/blog_api
 
 go 1.24.4
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- handlers/hanlers.go
+++ handlers/hanlers.go
@@ -7,11 +7,7 @@ import (
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Hello, World!\n")
-	} else {
-		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Hello, World!\n")
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
